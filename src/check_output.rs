f08d1d8897f391f458d3e4ad7e2b8b01f0778dc9
sqzume <173809390+sqzume@users.noreply.github.com>
2026-01-02 11:21:55 +0900

    refactor: テストの処理を関数に分離し、パスのフォーマットを変更

--- src/check_output.rs
+++ src/check_output.rs
@@ -9,26 +9,20 @@ fn count_files(problem: &str) -> Result<usize, std::io::Error> {
     Ok(count)
 }
 
-pub fn run(problem: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let files_num = count_files(problem)?;
-    let path = format!("tests/{}", problem);
+fn check_main(sample_path: &str, sample_num: usize) -> Result<(), Box<dyn std::error::Error>> {
+    // path を指定
+    let in_path = format!("{}/sample_{}.in", sample_path, sample_num);
+    let out_path = format!("{}/sample_{}.out", sample_path, sample_num);
 
-    let in_path = format!("{}/sample_1.in", path);
-    let out_path = format!("{}/sample_1.out", path);
-
-    // in.txt を読み込む
+    // sample の内容を読み込む
     let input_content = read_to_string(&in_path)?;
-    // out.txt を読み込む
     let output_content = read_to_string(&out_path)?;
 
-    // in.txt を標準入力として a.out を実行
-    // [TODO] パスの指定方法をカレントディレクトリに基づくようにする
+    // sample を標準入力として a.out を実行
     let input = File::open(&in_path)?;
-    let output = Command::new("test_sample/a.out")
-        .stdin(Stdio::from(input))
-        .output()?;
+    let output = Command::new("./a.out").stdin(Stdio::from(input)).output()?;
 
-    // サンプルケースを表示
+    // sample を表示
     println!("{}{}{}", "\x1b[34m", "[入力]", "\x1b[0m");
     println!("{}", input_content.trim());
     println!();
@@ -43,12 +37,21 @@ pub fn run(problem: &str) -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", output_content.trim());
     println!();
 
-    // out.txt と実行結果を比較して、"AC" or "WA" を出力する
+    // sample と実行結果を比較して、"AC" or "WA" を出力する
     if output_content.trim() == String::from_utf8_lossy(&output.stdout).trim() {
         println!("{}{}{}", "\x1b[32m", "AC", "\x1b[0m");
     } else {
         println!("{}{}{}", "\x1b[31m", "WA", "\x1b[0m");
     }
+
+    Ok(())
+}
+
+pub fn run(problem: &str) -> Result<(), Box<dyn std::error::Error>> {
+    // let files_num = count_files(problem)? / 2;
+    let path = format!("tests/{}", problem);
+
+    check_main(&path, 1)?;
 
     Ok(())
 }
